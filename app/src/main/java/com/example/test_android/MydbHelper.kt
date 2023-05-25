61f584aa1498436d6413ee3094fd09e471c568ac
NHTHang38 <thangtb100@gmail.com>
2023-05-25 17:55:27 +0700

    fix gd

--- app/src/main/java/com/example/test_android/MydbHelper.kt
+++ app/src/main/java/com/example/test_android/MydbHelper.kt
@@ -9,13 +9,8 @@ class MydbHelper(context:Context) :SQLiteOpenHelper(context,"JOKESDB", null, 1 )
 
         p0?.execSQL("CREATE TABLE JOKES(JOKEID Integer PRIMARY KEY AUTOINCREMENT, CONTENT TEXT) ")
 
-        p0?.execSQL("INSERT INTO JOKES(CONTENT) VALUES('A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\"\n" +
-                "\n" +
-                "The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.\"\n" +
-                "\n" +
-                "The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.\"')")
+        p0?.execSQL("INSERT INTO JOKES(CONTENT) VALUES('A child asked his father, \"How were people born?\" So his father said, \"Adam and Eve made babies, then their babies became adults and made babies, and so on.\"The child then went to his mother, asked her the same question and she told him, \"We were monkeys then we evolved to become like we are now.\"The child ran back to his father and said, \"You lied to me!\" His father replied, \"No, your mom was talking about her side of the family.\"')")
         p0?.execSQL("INSERT INTO JOKES(CONTENT) VALUES('Teacher: \"Kids,what does the chicken give you?\" Student: \"Meat!\" Teacher: \"Very good! Now what does the pig give you?\" Student: \"Bacon!\" Teacher: \"Great! And what does the fat cow give you?\" Student: \"Homework!\"')")
-
         p0?.execSQL("INSERT INTO JOKES(CONTENT) VALUES('A housewife, an accountant and a lawyer were asked \"How much is 2+2?\" The housewife replies: \"Four!\". The accountant says: \"I think it s either 3 or 4. Let me run those figures through my spreadsheet one more time.\" The lawyer pulls the drapes, dims the lights and asks in a hushed voice, \"How much do you want it to be?\"')")
 
 
